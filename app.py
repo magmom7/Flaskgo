bc41b9b8ee60785b475ef4d019cbd25614d4992a
GoodDonkey <president20500@gmail.com>
2021-06-22 21:02:31 +0900

    recaptcha, upload enabled

--- app.py
+++ app.py
@@ -2,30 +2,46 @@ from flask import Flask, request, render_template, redirect, url_for
 from dto import UserDTO, BoardDTO
 from dao import UserDAO, BoardDAO
 import datetime
+from flask_recaptcha import ReCaptcha
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['RECAPTCHA_SITE_KEY'] = '6LdI60gbAAAAAO9llS8p0UrBetVojtlG-1kuGm-l'
+app.config['RECAPTCHA_SECRET_KEY'] = '6LdI60gbAAAAAPl2g8q9guBs2lFohFUnNNkdpLSQ'
+recaptcha = ReCaptcha(app)
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def get():
-    return render_template('index.html')
+    message = ''  # Create empty message
+    print(request.method)
+    if request.method == 'POST':  # Check to see if flask.request.method is POST
+        if recaptcha.verify():  # Use verify() method to see if ReCaptcha is filled out
+            message = 'Thanks for filling out the form!'  # Send success message
+        else:
+            message = 'Please fill out the ReCaptcha!'  # Send error message
+    return render_template('index.html', message=message)
+
 
 @app.route('/membership', methods=['GET', 'POST'])
-def membership():   
+def membership():
     return render_template('member.html')
+
 
 @app.route('/insert', methods=['POST'])
 def userinsert():
     global index_user_counter
- 
+
     index_user_counter += 1
 
-    dto = UserDTO(index_user_counter, request.form.get('user_name'), request.form.get('user_pw'), request.form.get('user_interest'))
-    dao = UserDAO().userinsert(dto)
-
+    dto = UserDTO(index_user_counter, request.form.get('user_name'),
+                  request.form.get('user_pw'), request.form.get('user_interest'))
+    dao = UserDAO().userinsert(dto)  # 이 dao는 사용이 안됨
 
     # print("----77-----")
 
-    return  render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['post'])
 def userlogin():
@@ -34,30 +50,48 @@ def userlogin():
     # print("-"*30)
     # print(request.form.get('user_name'))
     # print("-"*30)
-    
+
     return UserDAO().userall()
+
 
 @app.route('/menu', methods=['get'])
 def getmenu():
     return render_template('menu.html')
 
-@app.route('/todolist', methods = ['get'])
+
+@app.route('/todolist', methods=['get'])
 def gettodolist():
     return render_template('todolist.html')
 
 
-@app.route('/list', methods = ['get'])
+@app.route('/list', methods=['get'])
 def getlist():
     return render_template('list.html')
 
-@app.route('/char2', methods = ['get'])
+
+@app.route('/char2', methods=['get'])
 def getchar2():
     return render_template('char2.html')
 
-@app.route('/todolist/gettext', methods = ['post'])
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@app.route('/fileUpload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        # 저장할 경로 + 파일명
+        f.save(secure_filename(f.filename))
+        return "현재 디렉토리에 - "+f.filename+" - 업로드 성공!!!"
+
+
+@app.route('/todolist/gettext', methods=['post'])
 def gettext():
     global index_text_counter
-    global uname 
+    global uname
     # print(f"로그인 된 이름 : {uname}")
     # print(type(uname))
     index_text_counter += 1
@@ -65,17 +99,19 @@ def gettext():
     # print(uid)
     # print(uid[0])
     now = datetime.datetime.now()
-    # print(now)          
+    # print(now)
     # 2021-06-22 12:43:19.673801
     nowDate = now.strftime('%Y-%m-%d')
     # print(nowDate)
     # print(type(nowDate))
-    dto = BoardDTO(index_text_counter, request.form.get('user_title'), request.form.get('user_text'), nowDate, uid[0])
+    dto = BoardDTO(index_text_counter, request.form.get(
+        'user_title'), request.form.get('user_text'), nowDate, uid[0])
     print(dto)
     BoardDAO().textinsert(dto)
     return request.form.get('user_title')
 
-@app.route('/list/showlist', methods = ['post'])
+
+@app.route('/list/showlist', methods=['post'])
 def showtext():
     # print("in")
     global uname
@@ -86,7 +122,8 @@ def showtext():
     # print(data)
     return data
 
-if __name__ == "__main__": 
-    index_user_counter = 0 # 초기값 받아오는 걸로 수정하기!
+
+if __name__ == "__main__":
+    index_user_counter = 0  # 초기값 받아오는 걸로 수정하기!
     index_text_counter = 0
-    app.run(debug=True, host = "127.0.0.1", port = "5000")
+    app.run(debug=True, host="127.0.0.1", port="5000")
--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 -- 임시로 만든 test 결과 확인
-
-drop table Users; 
+-- 참조 때문에 cascade 넣어서 드랍하였음
+drop table Users CASCADE CONSTRAINTS;
 drop table Board;
 drop sequence seq_user;
 drop sequence seq_board;
@@ -9,15 +9,20 @@ create sequence seq_board;
 
 create table Users(
     User_id number(3) primary key,
-    User_name varchar2(20) not null,
+    User_name nvarchar2(20) not null,
     User_secret number(20),
-    User_interest varchar2(20) constraint ck_user_interest check (User_interest in ('front', 'back', 'analysis'))
+    User_interest nvarchar2(20) constraint ck_user_interest check (User_interest in ('front', 'back', 'analysis'))
 );
 
 insert into Users values(seq_user.nextval, '유지현', 13,'front');
 insert into Users values(seq_user.nextval, '이성규', 15,'back');
 insert into Users values(seq_user.nextval, '장동기', 17,'analysis');
-insert into Users values(seq_user.nextval, '장동기', 17,'as');
+insert into Users values(seq_user.nextval, '장동기', 17,'as'); -- 이건 안들어감 
+
+-- insert into Users values(seq_user.nextval, 'jihyeon', 13,'front');
+-- insert into Users values(seq_user.nextval, 'seongkyu', 15,'back');
+-- insert into Users values(seq_user.nextval, 'donkey', 17,'analysis');
+-- insert into Users values(seq_user.nextval, 'donkey', 17,'as'); -- 이건 안들어감 
 
 
 create table Board(
@@ -29,3 +34,45 @@ create table Board(
 );
 
 insert into Board values(seq_board.nextval, '제목', '내용..........', sysdate, 1);
+
+
+
+
+
+
+
+-- 아래는 오라클 한글 설정 관련인데 안먹힘
+select * from v$nls_parameters;
+SELECT * FROM sys.props$ where name='NLS_LANGUAGE';
+select name, value$ from SYS.props$
+
+
+update sys.props$ set value$='UTF8' where name = 'NLS_CHARACTERSET';
+update sys.props$ set value$='KO16MSWIN949' where name='NLS_CHARACTERSET';  
+update sys.props$ set value$='KO16MSWIN949' where  name='NLS_NCHAR_CHARACTERSET';
+update sys.props$ set value$='KOREAN_KOREA.KO16MSWIN949' where name='NLS_LANGUAGE';
+
+commit;
+
+SHUTDOWN IMMEDIATE;
+
+STARTUP MOUNT;
+
+ALTER SYSTEM ENABLE RESTRICTED SESSION;
+
+ALTER SYSTEM SET JOB_QUEUE_PROCESSES=0;
+
+ALTER SYSTEM SET AQ_TM_PROCESSES=0;
+
+ALTER DATABASE OPEN;
+
+ALTER DATABASE CHARACTER SET INTERNAL_USE KO16MSWIN949;
+
+SHUTDOWN IMMEDIATE;
+
+STARTUP;
+
+
+
+출처: https://rumblefish.tistory.com/66 [RumbleFish]
+
